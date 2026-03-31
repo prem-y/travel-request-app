323b7767b40500d94067d359b18723fc94bde3b0
Prem Y <premy.connect@gmail.com>
2026-03-31 14:50:55 +0000

    Updated controller to handle Drawer and Navigation across view

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,31 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
-  "use strict";
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
 
-  return BaseController.extend("com.travel.request.travelrequest.controller.App", {
-      onInit() {
-      }
-  });
+    return Controller.extend("com.travel.request.travelrequest.controller.App", {
+
+        onInit: function () {
+            // Select Dashboard by default
+            var oSideNav = this.byId("sideNavigation");
+            var oNavList = this.byId("navList");
+            oSideNav.setSelectedItem(oNavList.getItems()[0]);
+            this._navigateTo("dashboard");
+        },
+
+        onToggleSideNav: function () {
+            var oToolPage = this.byId("toolPage");
+            oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+        },
+
+        onNavItemSelect: function (oEvent) {
+            var sKey = oEvent.getParameter("item").getKey();
+            this._navigateTo(sKey);
+        },
+
+        _navigateTo: function (sKey) {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo(sKey);
+        }
+    });
 });
