61cfd4d66822ee8ab0461ed9371f8d23e534198c
Prem Y <premy.connect@gmail.com>
2026-04-01 08:24:38 +0000

    Mapping value to all KPI

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,11 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
+
     return Controller.extend("com.travel.request.travelrequest.controller.Dashboard", {
+
         onInit: function () {
-            var oRouter = this.getOwnerComponent().getRouter();
-            oRouter.getRoute("dashboard").attachPatternMatched(this._onRouteMatched, this);
+            this.getOwnerComponent().getRouter()
+                .getRoute("dashboard")
+                .attachPatternMatched(this._onRouteMatched, this);
         },
-    })
-})
+
+        _onRouteMatched: function () {
+            this._refreshKPIs();
+        },
+
+        _refreshKPIs: function () {
+            var oSharedModel = this.getOwnerComponent().getModel("sharedModel");
+            var aRequests    = oSharedModel.getProperty("/requests") || [];
+
+            var oKpi = {
+                total:    aRequests.length,
+                pending:  aRequests.filter(function (r) { return r.status === "Pending";  }).length,
+                approved: aRequests.filter(function (r) { return r.status === "Approved"; }).length,
+                rejected: aRequests.filter(function (r) { return r.status === "Rejected"; }).length
+            };
+
+            var oKpiModel = this.getView().getModel("kpiModel");
+            if (!oKpiModel) {
+                oKpiModel = new JSONModel(oKpi);
+                this.getView().setModel(oKpiModel, "kpiModel");
+            } else {
+                oKpiModel.setData(oKpi);
+            }
+        }
+
+    });
+});
