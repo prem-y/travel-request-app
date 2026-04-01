478c84fd7ee862abfffee3e7720d59a548b14978
Prem Y <premy.connect@gmail.com>
2026-04-01 03:04:33 +0000

    Updated controller for Module selection menu

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,35 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/ActionSheet",
+    "sap/m/Button"
+], function (Controller, ActionSheet, Button) {
     "use strict";
+
+    // Define menus per module
+    var oModuleMenus = {
+        Employee: [
+            { text: "Dashboard",     icon: "sap-icon://home",         key: "dashboard" },
+            { text: "Employee View", icon: "sap-icon://employee",     key: "employee" }
+        ],
+        Manager: [
+            { text: "Dashboard",    icon: "sap-icon://home",     key: "dashboard" },
+            { text: "Manager View", icon: "sap-icon://manager",  key: "manager" }
+        ],
+        Finance: [
+            { text: "Dashboard",    icon: "sap-icon://home",         key: "dashboard" },
+            { text: "Finance View", icon: "sap-icon://money-bills",  key: "finance" }
+        ]
+    };
 
     return Controller.extend("com.travel.request.travelrequest.controller.App", {
 
         onInit: function () {
+            // Default module
+            this._sCurrentModule = "Employee";
+            this._applyModule("Employee");
+
             // Select Dashboard by default
-            var oSideNav = this.byId("sideNavigation");
             var oNavList = this.byId("navList");
-            oSideNav.setSelectedItem(oNavList.getItems()[0]);
             this._navigateTo("dashboard");
         },
 
@@ -23,9 +43,72 @@ sap.ui.define([
             this._navigateTo(sKey);
         },
 
+        // Open ActionSheet to pick module
+        onModuleSwitchPress: function (oEvent) {
+            var oButton = oEvent.getSource();
+            var that = this;
+
+            if (!this._oModuleActionSheet) {
+                this._oModuleActionSheet = new ActionSheet({
+                    title: "Select Module",
+                    buttons: [
+                        new Button({
+                            text: "Employee",
+                            icon: "sap-icon://employee",
+                            press: function () { that._applyModule("Employee"); }
+                        }),
+                        new Button({
+                            text: "Manager",
+                            icon: "sap-icon://manager",
+                            press: function () { that._applyModule("Manager"); }
+                        }),
+                        new Button({
+                            text: "Finance",
+                            icon: "sap-icon://money-bills",
+                            press: function () { that._applyModule("Finance"); }
+                        })
+                    ]
+                });
+                this.getView().addDependent(this._oModuleActionSheet);
+            }
+
+            this._oModuleActionSheet.openBy(oButton);
+        },
+
+        // Apply module: update label + drawer menu
+        _applyModule: function (sModule) {
+            this._sCurrentModule = sModule;
+
+            // Update module label in toolbar
+            this.byId("moduleLabel").setText("| " + sModule);
+
+            // Get menu config for this module
+            var aMenuItems = oModuleMenus[sModule];
+            var oNavList = this.byId("navList");
+            var aNavItems = oNavList.getItems();
+
+            // Hide all items first
+            aNavItems.forEach(function (oItem) {
+                oItem.setVisible(false);
+            });
+
+            // Show + update items for selected module
+            aMenuItems.forEach(function (oMenuDef, i) {
+                if (aNavItems[i]) {
+                    aNavItems[i].setText(oMenuDef.text);
+                    aNavItems[i].setIcon(oMenuDef.icon);
+                    aNavItems[i].setKey(oMenuDef.key);
+                    aNavItems[i].setVisible(true);
+                }
+            });
+
+            // Always navigate to dashboard on module switch
+            this._navigateTo("dashboard");
+            oNavList.setSelectedItem(oNavList.getItems()[0]);
+        },
+
         _navigateTo: function (sKey) {
-            var oRouter = this.getOwnerComponent().getRouter();
-            oRouter.navTo(sKey);
+            this.getOwnerComponent().getRouter().navTo(sKey);
         }
     });
 });
